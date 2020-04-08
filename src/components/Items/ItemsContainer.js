82282356335eeb055cef2e2b21c67827dd276379
Carl Dungca <carl.dungca@yahoo.com>
2020-04-08 15:13:09 -0700

    Installed uuid and gave all games IDs

--- src/components/Items/ItemsContainer.js
+++ src/components/Items/ItemsContainer.js
@@ -1,35 +1,39 @@
 import React, { useEffect } from 'react';
 import ItemsCard from './ItemsCard';
 import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4} from 'uuid';
 
 export default function ItemsContainer() {
 	const dispatch = useDispatch();
 	const games = useSelector(state => state.games);
 
-	const gameCards = games.map((game, i) => <ItemsCard key={i} game={game}/>)
-
 	function retrieveGames() {
 	// Normally we'd put an async request in here and get the results
+	// Normally, ID would come directly from the database
 		let games = [
 			{
 				name: 'Doom Eternal',
 				price: 59.99,
-				img: 'https://picsum.photos/200'
+				img: 'https://picsum.photos/200',
+				id: uuidv4()
 			},
 			{
 				name: 'Animal Crossing: New Horizons',
 				price: 59.99,
-				img: 'https://picsum.photos/201'
+				img: 'https://picsum.photos/201',
+				id: uuidv4()
 			},
 			{
 				name: 'Beat Saber',
 				price: 29.99,
-				img: 'https://picsum.photos/202'
+				img: 'https://picsum.photos/202',
+				id: uuidv4()
 			},
 			{
 				name: 'God of War',
 				price: 39.99,
-				img: 'https://picsum.photos/203'
+				img: 'https://picsum.photos/203',
+				id: uuidv4()
 			}
 		]
 		return games
@@ -43,7 +47,7 @@ export default function ItemsContainer() {
 		<div>
 			<h1>Items</h1>
 			<div className="itemsContainer">
-				{gameCards}
+				{games.map((game) => <ItemsCard key={game.id} game={game}/>)}
 			</div>
 		</div>
 	)
